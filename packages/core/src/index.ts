595fcc14789a51258cfd1a0c7f99e35130d90e60
LittleSound <464388324@qq.com>
2022-07-30 17:06:54 +0800

    init

---
+++ packages/core/src/index.ts
@@ -0,0 +1,4 @@
+export function hello(msg: string) {
+  // eslint-disable-next-line no-console
+  console.log('hello', msg)
+}
---
+++ packages/example/vite.config.ts
@@ -0,0 +1,56 @@
+/// <reference types="vitest" />
+
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import Vue from '@vitejs/plugin-vue'
+import Pages from 'vite-plugin-pages'
+import Components from 'unplugin-vue-components/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import Unocss from 'unocss/vite'
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '~/': `${resolve(__dirname, 'src')}/`,
+      'image-picker-plugin': resolve(__dirname, '../core/src/index.ts'),
+    },
+    dedupe: ['vue'],
+  },
+  plugins: [
+    Vue({
+      reactivityTransform: true,
+    }),
+
+    // https://github.com/hannoeru/vite-plugin-pages
+    Pages(),
+
+    // https://github.com/antfu/unplugin-auto-import
+    AutoImport({
+      imports: [
+        'vue',
+        'vue/macros',
+        'vue-router',
+        '@vueuse/core',
+      ],
+      dts: true,
+      dirs: [
+        './src/composables',
+      ],
+      vueTemplate: true,
+    }),
+
+    // https://github.com/antfu/vite-plugin-components
+    Components({
+      dts: true,
+    }),
+
+    // https://github.com/antfu/unocss
+    // see unocss.config.ts for config
+    Unocss(),
+  ],
+
+  // https://github.com/vitest-dev/vitest
+  test: {
+    environment: 'jsdom',
+  },
+})
